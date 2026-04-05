02f473130cf998a033b032537d81f17cb316314e
Abdallah Mahmoud <abdallahmf8@gmail.com>
2026-04-05 22:29:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+ e.waitUntil(
+   caches.open('chickana-v1').then((cache) => cache.addAll([
+     '/',
+     '/index.html',
+     'jpg.58304.jpg'
+   ])),
+ );
+});
+
+self.addEventListener('fetch', (e) => {
+ e.respondWith(
+   caches.match(e.request).then((response) => response || fetch(e.request)),
+ );
+});
